55493b68e539b3a43a6b3aedfa6e00fafa36a226
Yee <cittie.yi@gmail.com>
2017-03-15 12:11:17 +0800

    Install flask

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(name='yysdata',
+      version='0.1',
+      description='Quickly search for yys data',
+      author='Yee Joen',
+      author_email='yi.jiang.gameloft@gmail.com',
+
+      install_requires=[
+          'flask-script',
+          'flask-bootstrap',
+          'flask-moment',
+          'flask-wtf',
+          'flask-sqlalchemy',
+          'flask-migrate',
+          'flask-mail',
+          'flask-login'
+      ]
+      )
