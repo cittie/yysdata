f884279df5e8f46304444b1bdda675b219460073
Yee <cittie.yi@gmail.com>
2017-03-16 22:04:35 +0800

    Add mission page in route.

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,13 @@
 from . import main
 from flask import render_template, redirect, url_for, flash, current_app, request
-from ..models import Shikigami
+from ..models import Shikigami, Mission
 
 @main.route('/')
 def index():
     shikigamis = Shikigami.query.order_by(Shikigami.rarity.desc())
     return render_template('index.html', shikigamis=shikigamis)
+
+@main.route('/missions')
+def show_missions():
+    missions = Mission.query.order_by(Mission.name)
+    return render_template('missions.html', missions=missions)
